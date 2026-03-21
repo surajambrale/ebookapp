deebd7f21da2f8e2eb61dc2499819e687b3d3098
surajambrale <surajambrale9003@gmail.com>
2026-03-21 22:48:28 +0530

    environment file added-final

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  API = 'http://localhost:5000';
+  API = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
