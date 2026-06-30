31868fe60d887e823b2e6fc1daf79216728cc03d
surajambrale <surajambrale9003@gmail.com>
2026-06-30 17:19:05 +0530

    added admin panel code

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -3,10 +3,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { environment } from '../../../environments/environment';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule , RouterModule],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
