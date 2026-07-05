98080aaec4c9012e5bad39ba6223b578ff16d939
surajambrale <surajambrale9003@gmail.com>
2026-07-05 14:50:36 +0530

    server.js

--- server.js
+++ server.js
@@ -441,36 +441,42 @@ const books = [
   {
     id: "1",
     name: "Complete Fat Loss Guide",
+    price: 49,
     image: "assets/images/fatloss-book.jpeg"
   },
 
   {
     id: "2",
     name: "1500-Calories Diet Plan",
+    price: 49,
     image: "assets/images/1500-cal-diet.jpg"
   },
 
   {
     id: "3",
     name: "Habits That Change Your Life",
+    price: 49,
     image: "assets/images/habits.jpg"
   },
 
   {
     id: "4",
     name: "Beginner Guide",
+    price: 49,
     image: "assets/images/beginner-guide.jpg"
   },
 
   {
     id: "5",
     name: "Diabetes Control",
+    price: 49,
     image: "assets/images/diabetes-control.jpg"
   },
 
   {
     id: "6",
     name: "PCOD / PCOS Guide",
+    price: 49,
     image: "assets/images/pcod.jpg"
   }
 
