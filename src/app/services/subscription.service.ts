14c45370c4073cad4c69e85ab62486e4e8141370
surajambrale <surajambrale9003@gmail.com>
2026-07-12 21:48:06 +0530

    url

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
 
-  api = 'https://ebookapp.onrender.com';
+  api = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
