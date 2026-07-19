ae6284b94c0571960bc360222da391161393f491
surajambrale <surajambrale9003@gmail.com>
2026-07-19 10:31:57 +0530

    dynamic header testing

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -2,26 +2,60 @@ import { CommonModule } from '@angular/common';
 import { Component, HostListener, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.prod';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,RouterModule,SidebarComponent],
+  imports: [CommonModule, RouterModule, SidebarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  isSidebarOpen=signal(false);
+  isSidebarOpen = signal(false);
   isScrolled = signal(false);
-  isMobile=signal(window.innerWidth<=768);
+  isMobile = signal(window.innerWidth <= 768);
+  API = environment.apiUrl;
 
-  toggleSidebar(){
-    if(this.isMobile()){
-      this.isSidebarOpen.update(value=> !value);
+  appSetting: any = {};
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+
+  this.loadAppSetting();
+
+}
+
+loadAppSetting() {
+
+  this.http.get<any>(`${this.API}/app-setting`)
+    .subscribe({
+
+      next: (res) => {
+
+        this.appSetting = res;
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+}
+
+  toggleSidebar() {
+    if (this.isMobile()) {
+      this.isSidebarOpen.update(value => !value);
     }
   }
 
-   closeSidebar = () => {
+  closeSidebar = () => {
     this.isSidebarOpen.set(false);
   };
 
