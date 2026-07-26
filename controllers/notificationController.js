cb46cdf6c3f632570d6539bd2157d3abff9dbe3e
surajambrale <surajambrale9003@gmail.com>
2026-07-26 12:57:19 +0530

    ss

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -4,13 +4,43 @@ const nodemailer = require('nodemailer');
 // Gmail Transport
 const transporter = nodemailer.createTransport({
 
-    service: 'gmail',
+    host: "smtp.gmail.com",
+
+    port: 587,
+
+    secure: false,
+
+    requireTLS: true,
+
+    family: 4,
+
+    connectionTimeout: 60000,
+
+    greetingTimeout: 60000,
+
+    socketTimeout: 60000,
 
     auth: {
 
         user: process.env.EMAIL_USER,
 
         pass: process.env.EMAIL_PASS
+
+    }
+
+});
+
+transporter.verify((err, success) => {
+
+    if (err) {
+
+        console.log("SMTP ERROR:", err);
+
+    }
+
+    else {
+
+        console.log("SMTP READY");
 
     }
 
