dc69c1d442168b1cb2ce76d246881bea54100027
surajambrale <surajambrale9003@gmail.com>
2026-08-13 20:56:16 +0530

    ss

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+// import { provideHttpClient } from '@angular/common/http';
+// import { provideRouter } from '@angular/router';
+// import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(), provideRouter(routes)  // ✅ MUST ADD
-  ]
-});
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideHttpClient(), provideRouter(routes)  // ✅ MUST ADD
+//   ]
+// });
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(
+  AppComponent,
+  appConfig
+)
+.catch(err => console.error(err));
