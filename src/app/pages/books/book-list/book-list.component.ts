a3911d0339554b8d854a0488c91b3d101b3480f9
surajambrale <surajambrale9003@gmail.com>
2026-08-22 20:39:39 +0530

    final-sss

--- src/app/pages/books/book-list/book-list.component.ts
+++ src/app/pages/books/book-list/book-list.component.ts
@@ -3,7 +3,7 @@ import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http'
-import { environment } from '../../../../environments/environment.prod';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-book-list',
